ee5a0dc679d8d5b94bc932a54f636ed303e2e4da
Alex <a.behlen@gmail.com>
2012-07-26 19:08:54 -0700

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BreweryTracker::Application.config.secret_token = 'a9f7e71113b8d03c202a07bd0cc927083013e7a07f7f00eacf1563b36a74717c9b9f765261e60599969fcf9be0faab9d695b84303c94a849efc1e906bab7bded'
