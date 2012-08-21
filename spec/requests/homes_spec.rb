dbc8f1bfd9f181cf76f2c920c6932f71951a7da5
Alex <a.behlen@gmail.com>
2012-08-20 22:57:08 -0700

    Add browse page and related tests

--- spec/requests/homes_spec.rb
+++ spec/requests/homes_spec.rb
@@ -5,15 +5,28 @@ describe "Home Controller" do
   describe "Home Page" do
   
     it "should have the h1 'Brewery Tracker'" do
-      visit '/home/index'
+      visit '/home/home'
       page.should have_selector('h1', :text => 'Brewery Tracker')
     end
 
     it "should have the title 'Home'" do
-      visit '/home/index'
+      visit '/home/home'
       page.should have_selector('title',
-                        :text => "Brewery Tracker | Home")
-      end
+                        :text => "Brewery Tracker")
+     end
+  end
+
+  describe "Browse Brewery Page" do
+    
+    it "should have the h1 content 'Browse Breweries'" do
+      visit '/home/browse'
+      page.should have_selector('h1', :text => 'Browse Breweries')
+    end
+
+    it "should have the the title 'Browse Breweries'" do
+      visit '/home/browse'
+      page.should have_selector('title', :text => "Brewery Tracker | Browse")
+    end
   end
 end
 
