da52fd344391b3c0a51d2c7f2952df46e7b6bfd0
Alex <a.behlen@gmail.com>
2012-12-26 18:45:30 -0700

    Make a basic User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,9 +11,13 @@
 #
 
 class User < ActiveRecord::Base
-  attr_accessible :email, :name, :state
+  attr_accessible :email, :name, :state, :password, :password_confirmation
+  has_secure_password
 
   before_save { |user| user.email = email.downcase }
+
   validates :name, presence: true, length: { maximum: 50 }
   validates :email, presence: true, uniqueness: { case_sensitive: false }
+  validates :password, presence: true, length: { minimum: 6 }
+  validates :password_confirmation, presence: true
 end
