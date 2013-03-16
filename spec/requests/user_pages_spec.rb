a6fc1a43ab46b11ac4f901bd0864c1af43867277
albs22 <a.behlen@gmail.com>
2013-03-16 12:23:19 -0700

    Finish user signup

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -28,19 +28,36 @@ describe "User pages" do
         it "should not create a user" do
             expect {click_button submit }.not_to change(User, :count)
         end
+        
+        describe "after submission" do
+          before { click_button submit }
+          
+          it { should have_selector('title', text: 'Sign up') }
+          it { should have_content('error') }
+        end  
     end
     
     describe "with valid information" do
         before do
             fill_in "Name",         with: "Bill Adama"
-            fill_in "Email",        with: "bill@galactica.gal"
+            fill_in "Email",        with: "user@example.com"
             fill_in "Password",     with: "foobar"
             fill_in "Confirmation", with: "foobar"
         end
 
         it "should create a user" do
-            expect { click_button submit }.to change(User, count).by(1)
+            expect { click_button submit }.to change(User, :count).by(1)
         end
+        
+        describe "after saving the user" do
+          before { click_button submit }
+           let(:user) { User.find_by_email('user@example.com') }
+           
+           it { should have_selector('title', text: user.name) }
+           it { should have_selector('div.alert.alert-success', text: 'Welcome') }
+           
+        end
+        
     end
   end 
 end
